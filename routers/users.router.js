47ee191ed6fd355a9c14f2bbb13933334f8d1a95
Difa Winanda Adliya <difwinda@gmail.com>
2024-12-18 14:28:50 +0700

    feat: vercel

--- routers/users.router.js
+++ routers/users.router.js
@@ -7,5 +7,6 @@ const authenticateToken = require("../middlewares/auth.middleware");
 router.post("/auth/register", userController.createUser);
 router.post("/auth/login", userController.login);
 router.get("/profile", authenticateToken, userController.getUserById);
+router.get("/transactions", authenticateToken, userController.getUserById);
 
 module.exports = router;
